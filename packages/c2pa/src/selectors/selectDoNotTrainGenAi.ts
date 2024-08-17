955e7e0e674b007a28575f34c336c6da18ead1c5
kodalegit <133059419+kodalegit@users.noreply.github.com>
2024-08-17 22:39:31 +0000

    add selector for Do Not Train assertion

---
+++ packages/c2pa/src/selectors/selectDoNotTrainGenAi.ts
@@ -0,0 +1,24 @@
+import { Manifest, Assertion } from 'c2pa';
+
+export interface TrainingMiningEntry {
+  use: 'allowed' | 'notAllowed' | 'constrained';
+  constraint_info?: string;
+}
+
+export interface TrainingMiningMap {
+  [key: string]: TrainingMiningEntry;
+}
+
+export interface TrainingMiningAssertion
+  extends Assertion<'c2pa.training-mining', { entries: TrainingMiningMap }> {}
+
+export function selectDoNotTrainGenAi(manifest: Manifest) {
+  const trainingMining = manifest.assertions.data.find(
+    (assertion): assertion is TrainingMiningAssertion =>
+      assertion.label === 'c2pa.training-mining',
+  );
+
+  const genAiTrainingAssertion =
+    trainingMining?.data.entries['c2pa.ai_generative_training'];
+  return genAiTrainingAssertion;
+}
